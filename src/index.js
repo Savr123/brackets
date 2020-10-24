b196c9bda346c391561375670ab36307773b5353
Savr123 <Sangievv@mail.ru>
2020-10-24 19:16:39 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  brackets = Object.fromEntries(bracketsConfig);
+
+  for (const x of str.split("")){
+    if (stack[0] === x) {
+      stack.shift();
+    }else if(brackets[x]){
+      stack.unshift(brackets[x]);
+    }else{
+      return false
+    }
+  }
+  return stack.length === 0
 }
